c947d465e47513d3445fda8cfe5130f3a0724261
Chung Vo <voducchung@gmail.com>
2021-12-04 12:43:02 +0700

    added env var GOOGLE_SYNC_DURATION

--- automation/ui/settings.js
+++ automation/ui/settings.js
@@ -3,3 +3,4 @@ export const TODOIST_API_TOKEN = Cypress.env('TODOIST_API_TOKEN')
 export const TODOIST_WEB_BASE_URL = Cypress.env('TODOIST_WEB_BASE_URL')
 export const TODOIST_USERNAME = Cypress.env('TODOIST_USERNAME')
 export const TODOIST_PASSWORD = Cypress.env('TODOIST_PASSWORD')
+export const GOOGLE_SYNC_DURATION = Cypress.env('GOOGLE_SYNC_DURATION') || 5000 // In miliseconds
